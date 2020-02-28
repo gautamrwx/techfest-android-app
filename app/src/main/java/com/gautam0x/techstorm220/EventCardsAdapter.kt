fa9cad034771a932ef445e2d895e204ab5798ecc
gautamrwx <gautam.404@protonmail.com>
2020-02-28 23:12:26 +0530

    final card swiper

--- app/src/main/java/com/gautam0x/techstorm220/EventCardsAdapter.kt
+++ app/src/main/java/com/gautam0x/techstorm220/EventCardsAdapter.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.github.islamkhsh.CardSliderAdapter
 import kotlinx.android.synthetic.main.event_type_thumbnail.view.*
@@ -26,6 +28,13 @@ class EventCardsAdapter (private val events : ArrayList<EventCardData>,val conte
             card_event_name.text=events[position].eventName
             card_short_desc.text=events[position].eventShortDesc
             card_event_thumb.setImageResource(events[position].eventThumbnail)
+            card_more_info_btn.setOnClickListener {
+                val intent = Intent(context,EventFullInformationActivity::class.java)
+                intent.putExtra("currentJsonFilename","event_data_gaming.json")
+                intent.putExtra("stackPosition",position)
+                context.startActivity(intent)
+            }
+
         }
     }
 
